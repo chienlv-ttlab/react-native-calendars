db76a38148cea2b20e7b6d1eeab7b759fcbd076d
quan.lh <quanlh@tokyotechlab.com>
2022-05-26 02:14:00 +0700

    build to dist folder

---
+++ dist/src/expandableCalendar/week.d.ts
@@ -0,0 +1,8 @@
+import { CalendarProps } from '../calendar';
+export declare type WeekProps = CalendarProps;
+declare const Week: {
+    (props: WeekProps): JSX.Element;
+    displayName: string;
+    propTypes: any;
+};
+export default Week;
